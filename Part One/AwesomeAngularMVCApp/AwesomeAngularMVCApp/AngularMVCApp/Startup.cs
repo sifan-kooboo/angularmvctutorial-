b6d87f1614e5d9af703e9edf218e4366798f5848
sifan-kooboo <sifan@kooboo.cn>
2016-05-08 23:12:31 +0800

    start

---
+++ Part One/AwesomeAngularMVCApp/AwesomeAngularMVCApp/AngularMVCApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularMVCApp.Startup))]
+namespace AngularMVCApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
