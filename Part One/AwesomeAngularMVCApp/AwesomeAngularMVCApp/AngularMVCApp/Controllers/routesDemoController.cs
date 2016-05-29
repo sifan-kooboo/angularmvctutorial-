a9d5af134b8ec72b1fccb6104f71962293897946
sifan-kooboo <sifan@kooboo.cn>
2016-05-30 00:26:55 +0800

    Fix front-end router

---
+++ Part One/AwesomeAngularMVCApp/AwesomeAngularMVCApp/AngularMVCApp/Controllers/routesDemoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularMVCApp.Controllers
+{
+    public class RoutesDemoController : Controller
+    {
+        //
+        // GET: /routesDemo/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult One()
+        {
+            return View();
+        }
+        public ActionResult Two()
+        {
+            return View();
+        }
+        public ActionResult Three()
+        {
+            return View();
+        }
+	}
+}
---
+++ Part One/AwesomeAngularMVCApp/AwesomeAngularMVCApp/AngularMVCApp/Scripts/Angular/AngularMVCApp.js
@@ -0,0 +1,17 @@
+﻿var AngularMVCApp = angular.module('AngularMVCApp', ['ngRoute']);
+AngularMVCApp.controller('LandingPageController', LandingPageController);
+
+var configFunction = function ($routeProvider) {
+    $routeProvider.
+        when('/routeOne', {
+            templateUrl: 'RoutesDemo/One'
+        })
+        .when('/routeTwo', {
+            templateUrl: 'RoutesDemo/Two'
+        })
+        .when('/routeThree', {
+            templateUrl: 'RoutesDemo/Three'
+        });
+}
+
+AngularMVCApp.config(configFunction);
